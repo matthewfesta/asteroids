a4d57916604e264a249e5fe98dc70ed7f94032da
Matthew Festa <festa.matthew@me.com>
2024-08-20 22:30:31 -0400

    create basic game loop and allow closing of window

--- src/main.py
+++ src/main.py
@@ -1,8 +1,18 @@
 import pygame
+import constants
 
 
 def main():
     print("Starting asteroids!")
+    pygame.init()
+    screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
