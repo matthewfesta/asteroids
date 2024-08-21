826e62848224fd6d6a0872388d7e4fa35ac85124
Matthew Festa <festa.matthew@me.com>
2024-08-21 08:22:45 -0400

    use pygame clock and delta time var in loop to reduce resources

--- src/main.py
+++ src/main.py
@@ -6,6 +6,8 @@ def main():
     print("Starting asteroids!")
     pygame.init()
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -13,6 +15,7 @@ def main():
                 return
         screen.fill((0, 0, 0))
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
