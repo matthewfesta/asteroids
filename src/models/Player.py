2e4c67736c9e302b4041847f86436906dfe800c3
Matthew Festa <festa.matthew@me.com>
2024-08-21 22:01:50 -0400

    add draw method

--- src/models/Player.py
+++ src/models/Player.py
@@ -1,21 +1,25 @@
 import pygame
+import CircleShape
+from src import constants
 
 
-class CircleShape(pygame.sprite.Sprite):
+class Player(CircleShape):
 
-    def __init__(self, x, y, radius):
-        if hasattr(self, "containers"):
-            super().__init__(self.containers)
-        else:
-            super().__init__()
+    def __init__(self, x, y):
+        super().__init__(x, y, constants.PLAYER_RADIUS)
+        self.field = pygame.Vector2(x, y)
+        self.rotation = 0
 
-        self.position = pygame.Vector2(x, y)
-        self.velocity = pygame.Vector2(0, 0)
-        self.radius = radius
+    def triangle(self):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        a = self.position + forward * self.radius
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+        return [a, b, c]
 
     def draw(self, screen):
-        # sub-classes must override
-        pass
+        pygame.draw.polygon(screen, (255, 255, 255), self.triangle(), 2)
 
     def update(self, dt):
         # sub-classes must override
