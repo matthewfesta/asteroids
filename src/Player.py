5886891d6d35ceeecc4ffdbd70bb6b6a6b7dd524
Matthew Festa <festa.matthew@me.com>
2024-08-23 22:42:43 -0400

    add update method

--- src/Player.py
+++ src/Player.py
@@ -22,9 +22,12 @@ class Player(CircleShape):
         pygame.draw.polygon(screen, (255, 255, 255), self.triangle(), 2)
 
     def update(self, dt):
-        # sub-classes must override
-        pass
+        keys = pygame.key.get_pressed()
 
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
 
     def rotate(self, dt):
         self.rotation += constants.PLAYER_TURN_SPEED * dt
